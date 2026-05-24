410ece6109754fbc1b8e8a020352b660ac43ce78
Sơn Nguyễn <tson.csp@gmail.com>
2026-05-23 21:58:20 -0700

    Update CORS middleware allowed origins

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://search-and-summarize-production.up.railway.app",
+        "https://search-frontend-phi.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
